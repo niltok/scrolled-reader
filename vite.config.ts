5c2c7d12cf0e7850a7e81541222e314fc17251a6
玩火 <niltok@163.com>
2022-03-01 00:38:54 +0800

    add cert gen

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { readFileSync } from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        https: {
+            key: readFileSync('cert/server.key'),
+            cert: readFileSync('cert/server.crt')
+        }
+    },
     build: {
-        outDir: '../docs',
+        outDir: 'dist',
         emptyOutDir: true,
         minify: true
     },
